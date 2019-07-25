e12c4e3114f27d0ae5d6668359161b376f62eb08
AseelL <aseel.sahmood3@gmail.com>
2019-07-25 19:47:26 +0300

    Co-authored-by: ِAmoodaa Shatali <amoodaa@gmail.com>

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-let digit1=document.querySelector('#msTens');
-let digit2=document.querySelector('#msHundreds');
-let digit3=document.querySelector('#secondOnes');
-let digit4=document.querySelector('#secondTens');
+let digit1 = document.querySelector('#msTens');
+let digit2 = document.querySelector('#msHundreds');
+let digit3 = document.querySelector('#secondOnes');
+let digit4 = document.querySelector('#secondTens');
 
-let count1=0;
-let count2=0;
-let count3=0;
-let count4=0;
+let msTens = 0;
+let msHundreds = 0;
+let secondOnes = 0;
+let secondTens = 0;
 
-digit1.textContent
-function counter(){
-
-for(var i=0;i<10;i++){
-    setTimeout(()=>{
-    digit1.textContent=count1;
-
-    if(i<10){
-        count1++;
-        
-    }else{
-        count1=0;
-    }
-},10);
-   }
-   for(var i=0;i<10;i++){
-
-   }
-
-counter();
+setInterval(() => {
+if(msTens<9){
+    msTens++;
+digit1.textContent=msTens;
+}else{
+    msTens=0;
+}
+}, 10);
+setInterval(() => {
+    if(msHundreds<9){
+msHundreds++;
+digit2.textContent=msHundreds;
+}else{
+    msHundreds=0;
+}
+}, 100);
+setInterval(() => {
+    if(secondOnes<9){
+        secondOnes++;
+        digit3.textContent=secondOnes;
+        }else{
+            secondOnes=0;
+        }
+}, 1000);
+setInterval(() => {
+    if(secondTens<9){
+        secondTens++;
+        digit4.textContent=secondTens;
+        }else{
+            secondTens=0;
+        }
+}, 10000);
